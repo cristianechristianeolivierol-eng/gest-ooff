01a99981aa3d100a7f40bb058e836f95a9e06955
Crisolivier <conviteconexoesmagia@gmail.com>
2026-04-17 00:11:36 -0300

    Instalando icones e corrigindo layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// O comentário abaixo desativa o aviso visual do VS Code
 // @ts-ignore
 import './index.css'; 
 
 const container = document.getElementById('root');
 
 if (!container) {
-  throw new Error('Elemento root não encontrado no index.html');
+  throw new Error('Elemento root não encontrado');
 }
 
 const root = createRoot(container);
